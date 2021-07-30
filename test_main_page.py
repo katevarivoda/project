cf6f20dcc490f22db36f6c56e338cd5409434fbd
DESKTOP-TCH1DCU\user <katevarivoda@gmail.com>
2021-07-30 22:02:10 +0300

    Updated tests, added locators.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,15 @@
 from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.login_page import LoginPageLocators
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+
     page.open()  # открываем страницу
     page.go_to_login_page()
-
-
+    page = LoginPage(browser, url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/")
+    page.should_be_login_page()
