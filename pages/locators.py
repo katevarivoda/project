19e3b9e70c152108c2fd4ea578abd4864a1ae1ad
DESKTOP-TCH1DCU\user <katevarivoda@gmail.com>
2021-08-09 17:09:08 +0300

    Added test for adding product to basket

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,11 @@ class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
 
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.XPATH, "//button[contains(text(),'Add to basket')]")
+    BOOK_NAME_ADDED_TO_CART = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    BOOK_NAME = (By.TAG_NAME, "h1")
+    VIEW_BASKET = (By.XPATH, "//a[contains(text(),'View basket')]")
+    BOOK_PRICE = (By.XPATH, "//p[@class='price_color']")
+    PRICE_ADDED_TO_CART = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
