7d85a837f9aea569f06d0a9f902713b007d3bfb0
DESKTOP-TCH1DCU\user <katevarivoda@gmail.com>
2021-08-10 14:18:41 +0300

    Updated test to check different links

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,7 +6,7 @@ from selenium.common.exceptions import NoAlertPresentException
 
 class BasePage():
 
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=100):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
--- pages/product_page.py
+++ pages/product_page.py
@@ -19,9 +19,10 @@ class ProductPage(BasePage):
     def book_name_is_correct(self):
         book_name_success_message = (self.browser.find_element(*ProductPageLocators.BOOK_NAME_ADDED_TO_CART)).text
         book_name = (self.browser.find_element(*ProductPageLocators.BOOK_NAME)).text
-        assert book_name_success_message == book_name
+        assert book_name_success_message == book_name, "Book title and message book title are different"
 
     def book_price_is_correct(self):
-        book_price_success_message = (self.browser.find_element(*ProductPageLocators.PRICE_ADDED_TO_CART)).text
+        book_price_success_message = (self.browser.find_element(*ProductPageLocators.PRICE_ADDED_TO_CART)).get_attribute("innerText")
         book_price = (self.browser.find_element(*ProductPageLocators.BOOK_PRICE)).text
-        assert book_price_success_message == book_price
+        assert book_price_success_message == book_price, "Book price and basket price are different"
+
